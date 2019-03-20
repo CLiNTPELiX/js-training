bd9c6ab2faddf3c9c0840216aece8dc767198655
Dan CARBONE <dan.carbone@live.fr>
2019-03-20 22:39:42 +0100

    TDD3

--- exercises/get-length.js
+++ exercises/get-length.js
@@ -8,10 +8,9 @@
  */
 
 // Your code :
-
+const getLength = arg1 => arg1.length
 //* Begin of tests
 const assert = require('assert')
-
 assert.strictEqual(getLength([ 2, 42 ]), 2)
 assert.strictEqual(getLength([ 'pouet', 4, true ]), 3)
 assert.strictEqual(getLength(Array(100)), 100)
